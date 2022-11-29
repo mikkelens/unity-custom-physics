a726d8e90dd932b5074b24624e8e51cb1563708a
Mikkel <Mikkel123sej@gmail.com>
2022-11-29 14:19:20 +0100

    added a pushpriority field in settings

--- PhysicsSettings.cs
+++ PhysicsSettings.cs
@@ -6,13 +6,14 @@ namespace Physics
 	[CreateAssetMenu(fileName = "New PhysicsSettings", menuName = "PhysicsSettings")]
 	public class PhysicsSettings : ScriptableObject
 	{
-		[field:SerializeField] public float MinGroundNormalHeight { get; private set; } = 0.5f; // idk
-		[field:SerializeField] public float MinimumMoveDistance { get; private set; } = 0.001f; // idk
-		[field:SerializeField] public int MaxCollisionPreventionIterations { get; private set; } = 5; // idk
-		[field:SerializeField] public int MaxRegisteredCollidersPerIteration { get; private set; } = 5; // idk
-		[field:SerializeField] public Optional<LayerMask> CustomCollisionLayers { get; private set; } // defaulted during serialization
-		[field:SerializeField] public Optional<float> Gravity { get; private set; } = 25f; // positive value, applied downwards
-		[field:SerializeField] public Optional<float> Friction { get; private set; } = 0.25f;
-		[field:SerializeField] public Optional<float> MinimumFriction { get; private set; } = 0.1f;
+		[field: SerializeField] public float MinGroundNormalHeight { get; private set; } = 0.5f; // idk
+		[field: SerializeField] public float MinimumMoveDistance { get; private set; } = 0.001f; // idk
+		[field: SerializeField] public int MaxCollisionPreventionIterations { get; private set; } = 5; // idk
+		[field: SerializeField] public int MaxRegisteredCollidersPerIteration { get; private set; } = 5; // idk
+		[field: SerializeField] public Optional<LayerMask> CustomCollisionLayers { get; private set; } // defaulted during serialization
+		[field: SerializeField] public Optional<float> Gravity { get; private set; } = 25f; // positive value, applied downwards
+		[field: SerializeField] public Optional<float> Friction { get; private set; } = 0.25f;
+		[field: SerializeField] public Optional<float> MinimumFriction { get; private set; } = 0.1f;
+		[field: SerializeField] public Optional<int> PushPriority { get; private set; } = 0;
 	}
 }
